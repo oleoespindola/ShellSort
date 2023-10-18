1c77a2efdd45ffe36cded21686f2ecde669e6520
Leo Espindola <leo0.moreirah@gmail.com>
2023-10-18 01:04:01 -0300

    add: generate random list and execution total time

--- shellsort.py
+++ shellsort.py
@@ -1,13 +1,19 @@
+import random
+import time
+
 class ShellSort:
-    
+        
     def sort(unordered_list: list) -> list:
-        '''Ordenação dos dados'''
+        '''Retorna a lista ordenada e o tempo de execução do código'''
 
         # Primeiro definimos os intervalos da sequeência Ciura
         ciura_gaps = [1750, 701, 301, 132, 57, 23, 10, 4, 1]
         
         # Obtemos o tamanho da lista
         list_size = len(unordered_list)
+        
+        # Guarda a hora de INÍCIO da execução da ordenação
+        start_time = time.time()
         
         for gap in ciura_gaps:
             for i in range(gap, list_size):
@@ -20,5 +26,16 @@ class ShellSort:
                     element_position -= gap
                     
                 unordered_list[element_position] = current_element
-                
-        return unordered_list
+               
+        # Guarda a hora FINAL da execução da ordenação
+        end_time = time.time()
+        
+        total_time = end_time - start_time
+
+        return unordered_list, total_time
+    
+    def generate_list(random_end: int, list_range: int) -> list:
+        '''Gera uma lista aleatória, em que list_range é o tamanho da lista e random_end é o número aleatório final'''
+        
+        return random.sample(list(range(list_range)), random_end)
+
